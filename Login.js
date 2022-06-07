88ed40881dc761b89203731e6f55d1d9387aba51
Elijah <trent6679@gmail.com>
2022-06-07 14:58:09 -0600

    add Login script

---
+++ Login.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
+
+const Login = () => {
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [oneTimePassword, setOneTimePassword] = useState(null);
+  
+    return (
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={setPhoneNumber}
+          value={phoneNumber}
+          placeholder="123-456-7890"
+          keyboardType="phone-pad"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setOneTimePassword}
+          value={oneTimePassword}
+          placeholder="Enter Phone Number"
+          keyboardType="numeric"
+          secureTextEntry={true}
+        />
+      </SafeAreaView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+  
+  export default Login;
